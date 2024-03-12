c3d6bd3535097e042819480c36f7c35c23005629
Krupis <lengsfeldkuba@gmail.com>
2024-03-12 18:14:52 +0100

    Done cv-02

--- cv-01/main.cpp
+++ cv-01/main.cpp
@@ -1,11 +1,11 @@
 #include "database.h"
 
 int main() {
-    Email e("jakub.leng@seznam.cz","petr.novak@gmail.com","Ahoj Petre");
+    Email e("jakub.leng@seznam.cz","petr.novak@gmail.com","Ahoj Petre ...");
     Person p("Jakub",22,183);
     Documents d("main","cpp",50);
     cout << "Email od:" << e.ShowFrom() << endl << "pro:" << e.ShowTo()<<endl << "obsah:" << e.ShowMessage() << endl;
     cout << "Jmeno Vlastnika emailu:" << p.ShowName() << endl << "Vek:" << p.ShowAge() << endl << "Vyska:" << p.ShowHeight() << endl;
-    cout << "Dokument:" << d.ShowName() << "." << d.ShowSuffix() << " ma " << d.ShowPages() << " radku" << endl;
+    cout << "Dokument:" << d.ShowName() << "." << d.ShowSuffix() << " ma " << d.ShowPages() << " kB" << endl;
     return 0;
 }
